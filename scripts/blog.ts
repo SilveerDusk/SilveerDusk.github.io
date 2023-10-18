e7a4ccca0ea7a4eae42eef669cc9f2ce6ca7a5ac
Jason <Jason1@jelincic.com>
2023-10-17 18:57:25 -0700

    feat: adds typescript for blog posts

---
+++ scripts/blog.ts
@@ -0,0 +1,44 @@
+type Blog = {
+  title: string;
+  date: string;
+  description: string;
+  slug: string;
+}
+
+const blogs: Blog[] = [
+  {
+    title: "test-1",
+    date: "10/22/23",
+    description: "the first test blog",
+    slug: "template"
+  },
+  {
+    title: "test-2",
+    date: "10/23/23",
+    description: "the second test blog",
+    slug: "template"
+  }
+];
+
+function getBlog() {
+  const blogList = document.getElementById("blog-list");
+
+  blogs.forEach(({slug, title, date, description}) => {
+    const link =  document.createElement("a");
+    link.href = `blogs/${slug}.html`;
+    link.innerHTML = "Read More";
+    const postPreview = document.createElement("div");
+    postPreview.classList.add("post-preview");
+    postPreview.innerHTML = `
+    <h2 class="post-title">${title}</h2>
+    <h3 class="post-subtitle">${date}</h3>
+    <p class="post-description">${description}</p>`;
+
+    postPreview.appendChild(link);
+    if(blogList){
+      blogList.appendChild(postPreview)
+    }
+  });
+}
+
+getBlog();
