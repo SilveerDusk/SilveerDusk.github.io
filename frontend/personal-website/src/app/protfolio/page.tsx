395af062cd2b8a4e094581c0828060a23a3334ab
Jason <Jason1@jelincic.com>
2023-11-04 23:51:47 -0700

    feat: fixes styling on the protfolio page

--- frontend/personal-website/src/app/protfolio/page.tsx
+++ frontend/personal-website/src/app/protfolio/page.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Protfolio() {
   return (<div>
 			<div className="centered">
         <h1 className="title">My Protfolio</h1>
-        <h2>I did a thing once... well maybe a few times.</h2>
+        <h2 className="sub-title">I did a thing once... well maybe a few times.</h2>
         <div className="project">
           <a href="index.html">
-            <Image src="/JasonWebsite.png" alt="Photo of the Website" width={600} height={600}/>
+            <Image className="project_img" src="/JasonWebsite.png" alt="Photo of the Website" width={500} height={500}/>
           </a>
-          <div className="project-details">
-            <h3 className="project-name">
+          <div className="project_details">
+            <h3 className="project_title">
               My Personal Website
             </h3>
-            <p className="project-description">
+            <p className="project_description">
               I made my own personal website using a mix of html and css.
             </p>
-            <a href="index.html">Check it Out!</a>
+            <Link className="project_link" href="/">Check it Out!</Link>
           </div>
         </div>
       </div>
