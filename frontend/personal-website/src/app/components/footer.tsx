accbb43e46d8d109065ff102c94f52515342ca39
Jason <Jason1@jelincic.com>
2023-11-05 00:06:18 -0700

    feat: adds footer

---
+++ frontend/personal-website/src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '@/components/footer.module.css'
+
+export default function Footer() {
+  return (
+		<footer className={style.footer}>© 2023 Jason Jelincic&#39;s Personal Website | All Rights Reserved</footer>
+  );
+}
