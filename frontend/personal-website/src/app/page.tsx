1f46a23379a7568fd69b55c2f54b4803af9b5c17
Jason <Jason1@jelincic.com>
2024-10-22 22:50:35 +0800

    feat: updates landing page

--- frontend/personal-website/src/app/page.tsx
+++ frontend/personal-website/src/app/page.tsx
@@ -8,12 +8,15 @@ export default function Home() {
         <h1 className={style.title}>Welcome to My Website!</h1>
         <div className={style.about}>
           <div className={style.about_image}>
-            <Image src="/Slackgotophotocopy.jpeg" alt="A Photo of Me" width={400} height={400} ></Image>
+            <Image src="/RandomSelfie.jpeg" alt="A Photo of Me" width={800} height={800} ></Image>
           </div>
           <div className={style.about_text}>
-            <p>My name is <strong>Jason Jelincic</strong>, I am a <em>second year</em> computer science student at <strong>Cal Poly SLO </strong>
-             with a minor in Chinese.</p> 
-            <p>I am the <em>Outreach officer</em> for <strong>CS+AI</strong> and excited for the year and the opportunities it will present.</p>
+            <p>My name is <strong>Jason Jelincic</strong>, I am a <em>third year</em> computer science student at <strong>Cal Poly SLO </strong>
+             with a minor in Chinese. This fall I am studying aboard in Taiwan at National Taiwan University (NTU) where I am finishing my chinese
+             minor while learning about Taiwans importance on a global scale!</p> 
+            <p>I am the also a <em>Vice President</em> for Cal Poly's Computer Science and Artifical Intelligence Club (<strong>CS+AI</strong>) 
+            and a tech lead for Hack4Impact's SLO Charter. 
+            I am excited for the year and the opportunities it will present and I hope you enjoy looking around my site!</p>
           </div>
         </div>
       </div>
